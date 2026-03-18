361c0a3aad5792e8e328d9ed2daab808c3e99dba
JacobBermudes <marriagesebers@gmail.com>
2026-03-18 22:59:48 +0500

    refactoring

--- main.go
+++ main.go
@@ -19,42 +19,7 @@ import (
 	"github.com/go-redis/redis/v8"
 )
 
-type Config struct {
-	Name   string `json:"name"`
-	Config string `json:"config"`
-}
 
-type HandshakeRequest struct {
-	DeviceID string `json:"device_id"`
-}
-
-type hsresp struct {
-	Token string `json:"token"`
-}
-
-type S_con struct {
-	Sid string `json:"id"`
-}
-
-type Is struct {
-	I1 string `json:"i1"`
-}
-type CrcsReq struct {
-	Name           string `json:"name"`
-	ApplyISettings bool   `json:"apply_i_settings"`
-	ISettings      Is     `json:"i_settings"`
-}
-
-type CrcsResp struct {
-	Client Clops `json:"client"`
-}
-type Clops struct {
-	Clid string `json:"id"`
-}
-
-type Getcfg struct {
-	Clncfg string `json:"clean_config"`
-}
 
 var rdb *redis.Client
 
